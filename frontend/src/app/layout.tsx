34887b6e733f802af8cff14863642335bd96e2b0
KirienzoEth <kirienzo.eth@gmail.com>
2024-01-12 15:10:21 +0900

    Add footer with links and donation address

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Providers } from './providers';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
+import Footer from './footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Analytics />
           <SpeedInsights />
         </Providers>
+        <Footer />
       </body>
     </html>
   );
